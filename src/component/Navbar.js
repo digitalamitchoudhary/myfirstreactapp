c4b3cbe8320db6239195ef66ea7c9cc1459e733b
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2024-08-10 20:09:46 +0530

    aa

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-export default function Navbar(props ) {
+export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
+  <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -29,17 +30,19 @@ export default function Navbar(props ) {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
-        
-      </ul>
+        </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-primary" type="submit">Search</button>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
+
+   
   )
 }
+
 Navbar.propTypes = {
   title: PropTypes.string,
   aboutText: PropTypes.string
